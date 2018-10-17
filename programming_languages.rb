f07fb5213ce81622e17f67fc62ae5f2381987c2c
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 17:59:21 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,2 @@
-def reformat_languages(languages)
- new_hash ={}
- languages.each do |style, language|
-  language.each do |name, attributes|
-  new_hash[name] ||= attributes
-  new_hash[name][:style]=[]
-  new_hash[name][:style]<<style
-end
-end 
-new_hash
-end
+require 'pry'
+
